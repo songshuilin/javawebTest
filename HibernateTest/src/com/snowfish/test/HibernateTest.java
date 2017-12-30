6ddd31bed00614c094185f9b2a76e6788c34345d
songshuilin <631659521@qq.com>
2017-12-31 00:27:09 +0800

    hql 查询

--- HibernateTest/src/com/snowfish/test/HibernateTest.java
+++ HibernateTest/src/com/snowfish/test/HibernateTest.java
@@ -1,9 +1,16 @@
 package com.snowfish.test;
 
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Property;
+import org.hibernate.criterion.Restrictions;
+import org.junit.jupiter.api.Test;
 
 import com.snowfish.cn.Student;
 
@@ -13,7 +20,7 @@ public class HibernateTest {
 		//add();
 		//update();
 		//delete() ;
-		select();
+		//select();
 	}
 	
 	//增
@@ -23,8 +30,10 @@ public class HibernateTest {
 		SessionFactory sessionFactory=config.buildSessionFactory();
 		Session session=sessionFactory.openSession();
 		Transaction transaction=  session.beginTransaction();
-		Student student=new Student(3l,"wangwu",23,"Yinyu");
-		session.save(student);
+		for (int i= 3; i < 20; i++) {
+			Student student=new Student(i,"wangwu"+i,20+i,"计算机"+i);
+			session.save(student);
+		}
 		transaction.commit();
 		session.close();
 		sessionFactory.close();  
@@ -66,8 +75,37 @@ public class HibernateTest {
 		SessionFactory sessionFactory=config.buildSessionFactory();
 		Session session=sessionFactory.openSession();
 		Transaction transaction=session.beginTransaction();
-		Student student=session.load(Student.class,2l);
+//		Student student=session.load(Student.class,2l);
+//		System.out.println(student);
+		String sql="from Student where s_id= ? ";
+		Query query=session.createQuery(sql);
+		query.setParameter(0, 6l);
+		List<Student> list=query.list();
+		for (Student student : list) {
+			System.out.println(student);
+		}
+		transaction.commit();
+		session.close();
+		sessionFactory.close();
+	}
+	
+	//查 
+	@Test
+	public   void selectCriteria() {
+		Configuration configuration=new Configuration();
+		Configuration config=configuration.configure();
+		SessionFactory sessionFactory=config.buildSessionFactory();
+		Session session=sessionFactory.openSession();
+		Transaction transaction=session.beginTransaction();
+		//Property age = Property.forName("s_id");
+		Criteria criteria=session.createCriteria(Student.class);
+		criteria.add(Restrictions.eq("s_id", 10l));
+		Student student=(Student) criteria.uniqueResult();
 		System.out.println(student);
+		//List<Student> list=criteria.list();
+//		for (Student student : list) {
+//			System.out.println(student);
+//		}
 		transaction.commit();
 		session.close();
 		sessionFactory.close();
